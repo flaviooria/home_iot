a514c413c40be2569d038042a66ae6f3137b8b99
Flavio Oria <flavio.oria@bosonit.com>
2026-03-16 17:45:43 +0100

    ✨ Add model Telemetry

--- api/init_db.py
+++ api/init_db.py
@@ -7,7 +7,7 @@ if sys.platform == "win32":
 # ----------------------------
 
 from shared.database import engine, Base
-from shared.models import Installation, Zone, Device  # type: ignore
+from shared.models import Installation, Zone, Device, Telemetry  # type: ignore
 
 
 async def init_db():
@@ -15,10 +15,3 @@ async def init_db():
         print("🚀 Conectando a PostgreSQL...")
         await conn.run_sync(Base.metadata.create_all)
         print("✅ Estructura de 'home' creada correctamente.")
-
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(init_db())
-    except Exception as e:
-        print(f"❌ Error al inicializar la base de datos: {e}")
