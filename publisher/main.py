b6ecb3434b3b4ed9a715101cf9d0e515b0a41089
Flavio Oria <flavio.oria@bosonit.com>
2026-03-16 17:41:46 +0100

    ♻️ Establish a connection and generate metrics using the publishing agent. The subscription agent will be created; it listens to the topics and stores the metrics in the database.

--- publisher/main.py
+++ publisher/main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from publisher!")
+import asyncio
+import sys
+
+from shared.logger import setup_logger
+from shared.mqtt_settings import MqttSettings
+from src.publisher import PublisherAgent
+
+log = setup_logger("Publisher")
+
+
+async def start_agent():
+    try:
+        settings = MqttSettings()  # type: ignore
+
+        agent = PublisherAgent(
+            mqtt_broker=settings.mqtt_broker,
+            mqtt_port=settings.mqtt_port,
+            mqtt_username=settings.mqtt_username,
+            mqtt_password=settings.mqtt_password,
+            interval_minutes=settings.interval_minutes,
+            tz=settings.tz,
+            installation_prefix=settings.installation_prefix,
+        )
+
+        log.info("🚀 Iniciando Publisher Agent Asíncrono...")
+        await agent.run()
+
+    except Exception as e:
+        log.exception(f"💥 Error fatal: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(start_agent())
+    except KeyboardInterrupt:
+        log.info("👋 Agente detenido.")
--- subscriber/main.py
+++ subscriber/main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from subscriber!")
+import asyncio
+import sys
+
+from shared.logger import setup_logger
+from shared.mqtt_settings import MqttSettings
+from src.subscriber import SubscriberAgent
+
+log = setup_logger("Subscriber")
+
+
+async def start_agent():
+    try:
+        settings = MqttSettings()  # type: ignore
+
+        subscriber = SubscriberAgent(
+            mqtt_broker=settings.mqtt_broker,
+            mqtt_port=settings.mqtt_port,
+            mqtt_username=settings.mqtt_username,
+            mqtt_password=settings.mqtt_password,
+        )
+
+        log.info("🚀 Iniciando Subscriber Agent Asíncrono...")
+
+        await subscriber.run()
+    except Exception as e:
+        log.exception(f"💥 Error fatal: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(start_agent())
+    except KeyboardInterrupt:
+        log.info("👋 Agente detenido.")
