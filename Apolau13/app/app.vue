c3fdd8ab28b7f16e1d4a8cd43261a4815277cf60
Flavio Oria <flavio.oriap@gmail.com>
2026-04-19 16:55:26 +0200

    ✨ Init project frontend

---
+++ Apolau13/app/app.vue
@@ -0,0 +1,8 @@
+import { NuxtLayout } from '../.nuxt/components';
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ Apolau13/app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Holaa que tal</h1>
+</template>
+<script setup lang="ts">
+
+definePageMeta({
+    layout: 'default'
+})
+
+</script>
